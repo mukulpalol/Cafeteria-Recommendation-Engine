76f45025fd6c2be38fb5afff9457271887fcd841
mukul.palol <mukul.palol@intimetec.com>
2024-07-01 21:25:59 +0530

    updated notification types in table and enum

---
+++ CafeRecommendationSystem/CafeteriaRecommendationSystem.DAL/Migrations/20240701155523_updatedNotificationType.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CafeteriaRecommendationSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedNotificationType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2024, 6, 16, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9270));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2024, 6, 18, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9300));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2024, 6, 19, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9302));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "Date",
+                value: new DateTime(2024, 6, 20, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9303));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 5,
+                column: "Date",
+                value: new DateTime(2024, 7, 1, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9319));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 6,
+                column: "Date",
+                value: new DateTime(2024, 6, 17, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9333));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 7,
+                column: "Date",
+                value: new DateTime(2024, 6, 19, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9335));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 8,
+                column: "Date",
+                value: new DateTime(2024, 6, 20, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9336));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 9,
+                column: "Date",
+                value: new DateTime(2024, 6, 18, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9338));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 10,
+                column: "Date",
+                value: new DateTime(2024, 6, 19, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9344));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 11,
+                column: "Date",
+                value: new DateTime(2024, 6, 17, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9346));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 12,
+                column: "Date",
+                value: new DateTime(2024, 6, 18, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9347));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 13,
+                column: "Date",
+                value: new DateTime(2024, 6, 20, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9349));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 14,
+                column: "Date",
+                value: new DateTime(2024, 6, 19, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9350));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 15,
+                column: "Date",
+                value: new DateTime(2024, 7, 1, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9352));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 16,
+                column: "Date",
+                value: new DateTime(2024, 6, 16, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9353));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 17,
+                column: "Date",
+                value: new DateTime(2024, 6, 18, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9354));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 18,
+                column: "Date",
+                value: new DateTime(2024, 6, 20, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9356));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 19,
+                column: "Date",
+                value: new DateTime(2024, 6, 19, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9357));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 20,
+                column: "Date",
+                value: new DateTime(2024, 6, 20, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9359));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 21,
+                column: "Date",
+                value: new DateTime(2024, 6, 17, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9360));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 22,
+                column: "Date",
+                value: new DateTime(2024, 6, 19, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9362));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 23,
+                column: "Date",
+                value: new DateTime(2024, 6, 20, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9363));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 24,
+                column: "Date",
+                value: new DateTime(2024, 6, 18, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9365));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 25,
+                column: "Date",
+                value: new DateTime(2024, 6, 19, 21, 25, 22, 789, DateTimeKind.Local).AddTicks(9366));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2024, 6, 16, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8118));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2024, 6, 18, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8151));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2024, 6, 19, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8154));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "Date",
+                value: new DateTime(2024, 6, 20, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8157));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 5,
+                column: "Date",
+                value: new DateTime(2024, 7, 1, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8180));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 6,
+                column: "Date",
+                value: new DateTime(2024, 6, 17, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8201));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 7,
+                column: "Date",
+                value: new DateTime(2024, 6, 19, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8204));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 8,
+                column: "Date",
+                value: new DateTime(2024, 6, 20, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8207));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 9,
+                column: "Date",
+                value: new DateTime(2024, 6, 18, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8210));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 10,
+                column: "Date",
+                value: new DateTime(2024, 6, 19, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8213));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 11,
+                column: "Date",
+                value: new DateTime(2024, 6, 17, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8215));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 12,
+                column: "Date",
+                value: new DateTime(2024, 6, 18, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8218));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 13,
+                column: "Date",
+                value: new DateTime(2024, 6, 20, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8221));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 14,
+                column: "Date",
+                value: new DateTime(2024, 6, 19, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8224));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 15,
+                column: "Date",
+                value: new DateTime(2024, 7, 1, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8227));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 16,
+                column: "Date",
+                value: new DateTime(2024, 6, 16, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8232));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 17,
+                column: "Date",
+                value: new DateTime(2024, 6, 18, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8235));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 18,
+                column: "Date",
+                value: new DateTime(2024, 6, 20, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8237));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 19,
+                column: "Date",
+                value: new DateTime(2024, 6, 19, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8240));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 20,
+                column: "Date",
+                value: new DateTime(2024, 6, 20, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8243));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 21,
+                column: "Date",
+                value: new DateTime(2024, 6, 17, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8246));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 22,
+                column: "Date",
+                value: new DateTime(2024, 6, 19, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8249));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 23,
+                column: "Date",
+                value: new DateTime(2024, 6, 20, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8252));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 24,
+                column: "Date",
+                value: new DateTime(2024, 6, 18, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8255));
+
+            migrationBuilder.UpdateData(
+                table: "Feedback",
+                keyColumn: "ID",
+                keyValue: 25,
+                column: "Date",
+                value: new DateTime(2024, 6, 19, 21, 19, 19, 0, DateTimeKind.Local).AddTicks(8258));
+        }
+    }
+}
